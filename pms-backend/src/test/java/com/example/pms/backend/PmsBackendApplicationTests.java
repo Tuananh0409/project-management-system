a7c8b49be0f781d78ad731d6d81af6ee5e2df7d2
DESKTOP-1VIGCT6\Admin <nguyendinhtuananh7@gmail.com>
2026-05-13 09:35:01 +0700

    be folder

---
+++ pms-backend/src/test/java/com/example/pms/backend/PmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pms.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
